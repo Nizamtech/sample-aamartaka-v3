d3c0a178d7dd6c5f6a3202325ee8fbfac4017cb2
Nizamtech <nizamtech@aamartaka.com>
2022-06-07 10:17:21 +0600

    list icon addedclassName= mt-1 text-xs

--- src/components/Mobile/States/ListCard.js
+++ src/components/Mobile/States/ListCard.js
@@ -7,38 +7,45 @@ const ListCard = () => {
         <h1 className="text-xl text-slate-800 font-bold">Junayed Ahmed</h1>
         <div className="flex justify-between items-center">
           {" "}
-          <h1 className="text-xl text-gray-500 ">Profession: Salaried</h1>{" "}
+          <h1 className="text-xl text-gray-500 "> Salaried</h1>{" "}
           {/* <h1 className="text-xl text-gray-500">Banani,Dhaka,1230</h1> */}
         </div>
         <hr className="my-3" />
-        <div className="grid grid-cols-2 place-items-start  my-8">
+        <div className="grid grid-cols-2 place-items-start  my-2">
           <div>
-            <h1 className=" text-gray-500">Company Name</h1>
-            <h1 className=" text-slate-800 font-bold">Asha Telecom ltd.</h1>
+            <h1 className="text-sm text-gray-500 ">Company Name</h1>
+            <h1 className="text-sm text-slate-800 font-bold">
+              Asha Telecom ltd.
+            </h1>
           </div>
           <div className="flex flex-col justify-start">
-            <h1 className=" text-gray-500">Salary</h1>
-            <h1 className=" text-slate-800 font-bold">12000 TK</h1>
+            <h1 className="text-sm text-gray-500">Salary</h1>
+            <h1 className="text-sm text-slate-800 font-bold">12000 TK</h1>
           </div>
         </div>
-        <div className="grid grid-cols-2 place-items-start  my-8">
+        <div className="grid grid-cols-2 place-items-start  my-2">
           <div>
-            <h1 className=" text-gray-500">Location</h1>
-            <h1 className=" text-slate-800 font-bold">Banani,Dhaka-1230</h1>
+            <h1 className="text-sm text-gray-500">Location</h1>
+            <h1 className="text-sm text-slate-800 font-bold">
+              Banani,Dhaka-1230
+            </h1>
           </div>
           <div className="flex flex-col justify-start">
-            <h1 className=" text-gray-500">Visit Date </h1>
-            <h1 className=" text-slate-800 font-bold"> 2022-06-05</h1>
+            <h1 className="text-sm text-gray-500">Visit Date </h1>
+            <h1 className="text-sm text-slate-800 font-bold"> 2022-06-05</h1>
           </div>
         </div>
-        <div className="grid grid-cols-2 place-items-start my-8">
+        <div className="grid grid-cols-2 place-items-start my-2">
           <div>
-            <h1 className=" text-gray-500">Schedule Date </h1>
-            <h1 className=" text-slate-800 font-bold">2022-06-19</h1>
+            <h1 className="text-sm text-gray-500">Schedule Date </h1>
+            <h1 className="text-sm text-slate-800 font-bold">2022-06-19</h1>
           </div>
           <div className="flex flex-col justify-start">
-            <h1 className=" text-gray-500">Schedule Time </h1>
-            <h1 className=" text-slate-800 font-bold"> 12:00:00 - 14:00:00</h1>
+            <h1 className="text-sm text-gray-500">Schedule Time </h1>
+            <h1 className="text-sm text-slate-800 font-bold">
+              {" "}
+              12:00:00 - 14:00:00
+            </h1>
           </div>
         </div>
 
@@ -53,42 +60,66 @@ https://i.ibb.co/WgnHDz3/icons8-sms-96.png
 https://i.ibb.co/fSyHNQ1/icons8-phone-96.png
          */}
         <div className="grid grid-cols-3 place-items-start my-8 gap-4">
-          <img
-            src="https://i.ibb.co/fSyHNQ1/icons8-phone-96.png"
-            alt=""
-            width="50px"
-            height="50px"
-          />
-          <img
-            src="https://i.ibb.co/WgnHDz3/icons8-sms-96.png"
-            alt=""
-            width="50px"
-            height="50px"
-          />
-          <img
-            src="https://i.ibb.co/4s6VLW1/icons8-furnitureandhousehold-64.png"
-            alt=""
-            width="50px"
-            height="50px"
-          />
-          <img
-            src="https://i.ibb.co/v3YzgNg/icons8-follow-up-64.png"
-            alt=""
-            width="50px"
-            height="50px"
-          />
-          <img
-            src="https://i.ibb.co/6rcLZzD/icons8-update-96.png"
-            alt=""
-            width="50px"
-            height="50px"
-          />
-          <img
-            src="https://i.ibb.co/chYZ6xm/icons8-forward-64.png"
-            alt=""
-            width="50px"
-            height="50px"
-          />
+          {/* call div  */}
+          <div className="flex flex-col  justify-center items-center">
+            <img
+              src="https://i.ibb.co/fSyHNQ1/icons8-phone-96.png"
+              alt=""
+              width="30px"
+              height="30px"
+            />
+            <small className=" mt-1 text-xs">Call</small>
+          </div>
+          {/* sms  */}
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src="https://i.ibb.co/WgnHDz3/icons8-sms-96.png"
+              alt=""
+              width="30px"
+              height="30px"
+            />
+            <small className=" mt-1 text-xs">SMS</small>
+          </div>
+          {/* Schedule  */}
+          <div className="flex flex-col  justify-center items-center">
+            <img
+              src="https://i.ibb.co/4s6VLW1/icons8-furnitureandhousehold-64.png"
+              alt=""
+              width="30px"
+              height="30px"
+            />
+            <small className=" mt-1 text-xs">Schedule</small>
+          </div>
+          {/* follow-up  */}
+          <div className="flex flex-col  justify-center items-center">
+            <img
+              src="https://i.ibb.co/v3YzgNg/icons8-follow-up-64.png"
+              alt=""
+              width="30px"
+              height="30px"
+            />
+            <small className=" mt-1 text-xs">follow Up</small>
+          </div>
+          <div className="flex flex-col  justify-center items-center">
+            <img
+              src="https://i.ibb.co/6rcLZzD/icons8-update-96.png"
+              alt=""
+              width="30px"
+              height="30px"
+            />
+            <small className=" mt-1 text-xs">Update</small>
+          </div>
+          {/* Forward  */}
+          <div className="flex flex-col  justify-center items-center">
+            {" "}
+            <img
+              src="https://i.ibb.co/chYZ6xm/icons8-forward-64.png"
+              alt=""
+              width="30px"
+              height="30px"
+            />
+            <small className=" mt-1 text-xs">Forward</small>
+          </div>
         </div>
 
         {/* 
