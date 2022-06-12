f089609736b7fbd559af8e4fe60102b0e12f99dc
Nizamtech <nizamtech@aamartaka.com>
2022-06-12 17:17:20 +0600

    capture buttion added

--- src/pages/test.js
+++ src/pages/test.js
@@ -7,7 +7,11 @@ const Test = () => {
     height: 720,
     facingMode: "user",
   };
-
+  const webcamRef = React.useRef(null);
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc);
+  }, [webcamRef]);
   return (
     <div>
       {" "}
@@ -15,22 +19,18 @@ const Test = () => {
       <div>
         <Webcam
           audio={false}
-          height={250}
+          height={500}
+          ref={webcamRef}
           screenshotFormat="image/jpeg"
-          width={250}
+          width="100%"
           videoConstraints={videoConstraints}
+        />
+        <button
+          className="my-4 bg-cyan-500 text-white px-3 py-2 rounded-md flext justify-center items-center mx-auto w-full"
+          onClick={capture}
         >
-          {({ getScreenshot }) => (
-            <button
-              onClick={() => {
-                const imageSrc = getScreenshot();
-                console.log(imageSrc);
-              }}
-            >
-              Capture photo
-            </button>
-          )}
-        </Webcam>
+          Capture photo
+        </button>
       </div>
     </div>
   );
