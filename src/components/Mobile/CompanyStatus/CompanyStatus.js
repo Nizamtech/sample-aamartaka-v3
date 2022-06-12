1675c0dc991cd97be3fa18434d2ba5ada1186750
Nizamtech <nizamtech@aamartaka.com>
2022-06-12 12:25:00 +0600

    company serach is done

--- src/components/Mobile/CompanyStatus/CompanyStatus.js
+++ src/components/Mobile/CompanyStatus/CompanyStatus.js
@@ -4,6 +4,42 @@ import AsyncSelect from "react-select/async";
 
 const CompanyStatus = () => {
   const [state, setState] = useState([]);
+  const [selected, setSelected] = useState();
+
+  const data = [
+    {
+      name: "Aamartaka",
+      bank: "City Bank",
+      categoryType: "Listed Company (Categorize)",
+      category: "Listed Company (Categorize)",
+      categoryName: "B",
+      companyType: "Financial Institution",
+    },
+    {
+      name: "Robi",
+      bank: "City Bank",
+      categoryType: "Listed Company (Categorize)",
+      category: "Listed Company (Categorize)",
+      categoryName: "A",
+      companyType: "Telecommunication",
+    },
+    {
+      name: "Grameenphone",
+      bank: "Asha Bank",
+      categoryType: "Listed Company (Categorize)",
+      category: "Listed Company (Categorize)",
+      categoryName: "A",
+      companyType: "Telecommunication",
+    },
+    {
+      name: "Lonka Bangla",
+      bank: "Grameen Bank",
+      categoryType: "Listed Company (Categorize)",
+      category: "Listed Company (Categorize)",
+      categoryName: "A",
+      companyType: "Money Transfer",
+    },
+  ];
 
   const colourOptions = [
     { label: "aamartaka" },
@@ -35,10 +71,15 @@ const CompanyStatus = () => {
     // setState(newValue);
   };
 
+  const handleChange = (newValue) => {
+    console.log(newValue);
+    setSelected(newValue);
+  };
+
   return (
     <div>
       <BackMenu title="Company Status" />
-      <h1>{state && state}</h1>
+      {/* <h1>{state && state}</h1> */}
 
       <div className="mx-1  my-">
         <AsyncSelect
@@ -47,55 +88,31 @@ const CompanyStatus = () => {
           loadOptions={loadOptions}
           defaultOptions
           onInputChange={handleInputChange}
-          onChange={(e) => console.log("Onchange Function", e)}
+          onChange={(e) => handleChange(e)}
         />
       </div>
 
-      {/* <form className="flex items-center m-1">
-        <label className="sr-only">Search</label>
-        <div className="relative w-full">
-          <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-            <svg
-              className="w-5 h-5 text-gray-500 dark:text-gray-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+      <div>
+        {selected && (
+          <div className="m-2 myShadow p-2 rounded-md font-exo">
+            <h1 cl>City Bank</h1>
+            <hr className=" border-dotted my-1 " />
+            <h1>{selected.label}</h1>
+            <h1 className="grid grid-cols-3 text-xs mt-1">
+              <span className=" text-slate-400">Category Type</span>
+              <span className=" col-span-2"> Listed Company (Categorize)</span>
+            </h1>
+            <h1 className="grid grid-cols-3 text-xs mt-1">
+              <span className=" text-slate-400">Category Name</span>{" "}
+              <span className=" col-span-2">B</span>
+            </h1>
+            <h1 className="grid grid-cols-3 text-xs mt-1">
+              <span className=" text-slate-400">Company Type</span>{" "}
+              <span className=" col-span-2">Other</span>
+            </h1>
           </div>
-          <input
-            type="text"
-            id="simple-search"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          <svg
-            className="w-5 h-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
-        </button>
-      </form> */}
+        )}
+      </div>
     </div>
   );
 };
