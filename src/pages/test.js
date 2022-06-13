f84e470c90fdc037ae898d3b0fbb97a37498070b
Nizamtech <nizamtech@aamartaka.com>
2022-06-13 11:24:32 +0600

    lock file deleted

--- src/pages/test.js
+++ src/pages/test.js
@@ -13,56 +13,10 @@ const Test = () => {
     const imageSrc = webcamRef.current.getScreenshot();
     console.log(imageSrc);
     setImgSrc(imageSrc);
-  }, [webcamRef,setImgSrc]);
+  }, [webcamRef, setImgSrc]);
   return (
     <div>
-      {" "}
-      <BackMenu title="This Test Page" />
-<<<<<<< HEAD
-      <div>
-        <div className="relative">
-          <Webcam
-            audio={false}
-            height={500}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            width="100%"
-            // videoConstraints={videoConstraints}
-          />
-        </div>
-        <button
-          className="absolute top-96 bg-blue-500 text-white p-2 rounded hover:bg-blue-800 m-2 my-4  px-3 py-2  flext justify-center items-center mx-auto w-full"
-          onClick={capture}
-        >
-          Capture photo
-        </button>
-      </div>
-=======
-    
-      {imgSrc ? 
-                <img
-                    src={imgSrc}
-                />
-            :<div>
-            
-              <Webcam
-                audio={false}
-                height={500}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                width="100%"
-                // videoConstraints={videoConstraints}
-              />
-            
-            <button
-              className="bg-blue-500 text-white p-2 rounded hover:bg-blue-800 m-2 my-4  px-3 py-2  flext justify-center items-center mx-auto w-full"
-              onClick={capture}
-            >
-              Capture photo
-            </button>
-            
-          </div>}
->>>>>>> 207b2eeee099f28d6bc7f88d6e2319f4ae68d28d
+      <h1>Test</h1>
     </div>
   );
 };
