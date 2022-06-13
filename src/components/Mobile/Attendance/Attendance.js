e764b35aa931190c1010159df1d9792976ee008a
Nizamtech <nizamtech@aamartaka.com>
2022-06-13 11:21:02 +0600

    captire buttion added

--- src/components/Mobile/Attendance/Attendance.js
+++ src/components/Mobile/Attendance/Attendance.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import BackMenu from '../../Shared/BackMenu';
-import WebCamera from './WebCamera';
+import React from "react";
+import BackMenu from "../../Shared/BackMenu";
+import WebCamera from "./WebCamera";
 
 const Attendance = () => {
-    return (
-        <div>
-            <BackMenu title= "Attendence "/>
-            <WebCamera />
-        </div>
-    );
+  // BASE64 IMAGE UPLOADING
+  const [base64, setBase64] = React.useState(null);
+  const [base64Error, setBase64Error] = React.useState(null);
+  const [base64Uploading, setBase64Uploading] = React.useState(false);
+  const [base64Uploaded, setBase64Uploaded] = React.useState(false);
+
+  const handleBase64Change = (e) => {
+    e.preventDefault();
+    setBase64(e.target.files[0]);
+    setBase64Error(null);
+  };
+
+  const handleBase64Upload = () => {
+    const formData = new FormData();
+    formData.append("image", base64);
+    setBase64Uploading(true);
+    fetch("/api/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setBase64Uploading(false);
+        setBase64Uploaded(true);
+      })
+
+      .catch((err) => {
+        setBase64Uploading(false);
+        setBase64Error(err);
+      });
+  };
+
+  return (
+    <div>
+      <BackMenu title="Attendence " />
+      <WebCamera handleBase64Upload={handleBase64Upload} />
+    </div>
+  );
 };
 
 export default Attendance;
--- src/components/Mobile/Attendance/WebCamera.js
+++ src/components/Mobile/Attendance/WebCamera.js
@@ -1,58 +1,66 @@
 import React, { useState } from "react";
 import Webcam from "react-webcam";
 import { useRouter } from "next/router";
-const WebCamera = () => {
-    const router = useRouter()
-    const webcamRef = React.useRef(null);
-    const [imgSrc, setImgSrc] = useState(null);
-    const capture = React.useCallback(() => {
-      const imageSrc = webcamRef.current.getScreenshot();
-      console.log(imageSrc);
-      setImgSrc(imageSrc);
-    }, [webcamRef,setImgSrc]);
-    return (
+const WebCamera = ({ handleBase64Upload }) => {
+  const router = useRouter();
+  const webcamRef = React.useRef(null);
+  const [imgSrc, setImgSrc] = useState(null);
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc);
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
+
+  return (
+    <div>
+      {imgSrc !== null ? (
         <div>
-           {imgSrc!==null ? 
-               <div>
-                    <img
-                    src={imgSrc}
-                />
-             <div className="flex justify-between items-center">
-             <button
+          <img src={imgSrc} />
+          <div className="flex justify-between items-center">
+            <button
               className="bg-blue-500 text-white p-2 rounded hover:bg-blue-800 m-2 my-4  px-3 py-2  flext justify-center items-center mx-auto w-40"
-              onClick={()=>setImgSrc(null)}
+              onClick={() => setImgSrc(null)}
             >
               Reset
             </button>
             <button
               className="bg-blue-500 text-white p-2 rounded hover:bg-blue-800 m-2 my-4  px-3 py-2  flext justify-center items-center mx-auto w-40 "
-              onClick={()=>router.push('/')}
+              onClick={() => handleBase64Upload}
             >
               Submit
             </button>
-             </div>
-               </div>
-            :<div>
-            
-              <Webcam
-                audio={false}
-                height={500}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                width="100%"
-                // videoConstraints={videoConstraints}
-              />
-            
-            <button
-              className="bg-blue-500 text-white p-2 rounded hover:bg-blue-800 m-2 my-4  px-3 py-2  flext justify-center items-center mx-auto w-full"
-              onClick={capture}
-            >
-              Capture photo
-            </button>
-            
-          </div>} 
+          </div>
         </div>
-    );
+      ) : (
+        <div>
+          <Webcam
+            className="relative"
+            audio={false}
+            height={"100%"}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width="100%"
+            // videoConstraints={videoConstraints}
+          />
+
+          <div className=" absolute bottom-0 w-full ">
+            <button
+              onClick={capture}
+              className=" flex justify-center items-center mx-auto"
+            >
+              <img
+                className="mx-auto"
+                width="100px"
+                height="100px"
+                src="https://i.ibb.co/gJ02ML6/icons8-unchecked-radio-button-96.png"
+                alt=""
+              />
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default WebCamera;
