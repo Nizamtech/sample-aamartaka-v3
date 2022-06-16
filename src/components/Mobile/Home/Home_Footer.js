dd0fe0234d73a328fc39b5ae65aae7baae3cf404
nizam <nizamtech@aamartaka.com>
2022-06-16 19:03:35 +0600

    footer almost done

--- src/components/Mobile/Home/Home_Footer.js
+++ src/components/Mobile/Home/Home_Footer.js
@@ -18,24 +18,30 @@ const Home_Footer = () => {
   return (
     <div className="absolute w-full mt-6 ">
       {/* <Footer_modal showModal={showModal} setShowModal={setShowModal} /> */}
-      <div className="border-t rounded-t-[25px] myShadow px-2 bottom-0 amin  bg-white   border-gray-200 shadow-md grid grid-cols-3 place-content-between place-items-center">
+      <div className="border-b rounded-b-[30px] myShadow px-2 bottom-0 amin  bg-white   border-gray-200 shadow-md grid grid-cols-3 place-content-between place-items-center">
         <div>
           <button
             onClick={handleAttendance}
-            className="text-xl font-exo2 font-bold"
+            className="text-xl font-exo2 font-bold flex  flex-col justify-center items-center my-1"
           >
-            ATTEND
+            <img
+              width="30px"
+              height="30px"
+              src="https://i.ibb.co/q90Gq33/icons8-attendance-64.png"
+              alt=""
+            />
+            <h1 className=" font-monster text-md text-slate-700">Attend</h1>
           </button>
         </div>
         <div className=" ">
           <button
             onClick={() => router.push("/lead/newlead")}
-            className=" p-4  relative top-[-15px]  py-1 px-2 z-50 flex flex-col justify-center items-center"
+            className=" p-4  relative top-[-29px]  py-1 px-2 z-50 flex flex-col justify-center items-center"
           >
             <img
               src="https://i.ibb.co/r6h3kWB/pluse.png"
               alt="Logo"
-              className="h-10 w-10 mr-2 myShadow2  rounded-[50%] "
+              className="h-12 w-12 mr-2 myShadow2  rounded-[50%] "
             />
             {/* <span className=" text-[10px] font-bold  font-roboto ">New</span>
             <span className=" text-[10px] font-bold  font-roboto ">Lead</span> */}
@@ -45,9 +51,15 @@ const Home_Footer = () => {
           {" "}
           <button
             onClick={() => () => router.push("/dashboard")}
-            className="text-xl font-exo2 font-bold"
+            className="text-xl font-exo2 font-bold flex  flex-col justify-center items-center my-1"
           >
-            STATS
+            <img
+              width="30px"
+              height="30px"
+              src="https://i.ibb.co/vHTPw8R/icons8-area-chart-64.png "
+              alt=""
+            />
+            <h1 className=" font-monster text-md text-slate-700">Stats</h1>
           </button>
         </div>
         <div></div>
--- src/components/Mobile/Home/Home_Header.js
+++ src/components/Mobile/Home/Home_Header.js
@@ -6,44 +6,45 @@ const Home_Header = () => {
       <div className="gradientBG font-exo rounded-lg border-b-0 rounded-b-none border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 p-3 ">
         <div>
           <div>
-            <h1 className="text-md  text-white font-monster uppercase font-bold tracking-wider ">
+            <h1 className="text-xs  text-white font-monster font-[16px]   ">
               Total Balance
             </h1>
-            <button className="widraw font-monster text-[#3A2424]">
+            <button className="widraw text-[10px] font-monster text-[#3A2424]">
               Widraw
             </button>
           </div>
-          <h1 className="text-xl font-monster text-white  ">BDT 1000</h1>
+          <h1 className="text-sm  text-white font-bold">BDT 10,000</h1>
         </div>
 
-        <div className="grid grid-cols-2 font-monster mt-1">
+        <div className="grid grid-cols-2 font-monster mt-4">
           <div>
-            <h1 className="text-sm text-white  tracking-wide font-bold ">
-              {" "}
+            <h1 className="text-xs text-white  tracking-wide  ">
               My Commission
             </h1>
-            <h1 className="text-lg text-white font-bold ">BDT 1200</h1>
+            <h1 className="text-sm  text-white font-bold  ">BDT 12,000</h1>
           </div>
           <div>
-            <h1 className="text-white text-sm  tracking-wide font-bold">
+            <h1 className="text-white text-xs  tracking-wide ">
               Referral Commission
             </h1>
-            <h1 className="text-lg text-white  font-bold ">BDT 100</h1>
+            <h1 className="text-sm text-white font-bold  ">BDT 10,000</h1>
           </div>
         </div>
 
         {/* <hr className=" border-dashed border-1 my-1" /> */}
       </div>
 
-      <div className="pt-3   pb-2 grid border-t-0 font-monster rounded-b-md grid-cols-2 place-content-between place-items-center shadow-md border-b  bg-gray-100 w-full">
-        <div className="flex flex-col justify-between items-center ">
-          <h1 className=" font-bold text-slate-500 font-roboto">
+      <div className="  pt-3 pb-2 grid border-t-0  rounded-b-md grid-cols-2 place-content-between place-items-center shadow-md border-b  bg-gray-50 w-full">
+        <div className="flex flex-col justify-between items-center w-full border-r  ">
+          <h1 className=" font-bold text-slate-500 font-monster ">
             Target Points
           </h1>
           <h1 className=" text-lg  text-black font-bold tracking-[2px]">15</h1>
         </div>
         <div className="flex flex-col justify-between items-center ">
-          <h1 className="font-bold text-slate-500 f">Achive Points</h1>
+          <h1 className="font-bold text-slate-500 font-monster">
+            Achive Points
+          </h1>
           <h1 className=" text-lg dont-exo text-black font-bold tracking-[2px]">
             10
           </h1>
