33a9c53840b47131e18cfe34e4f831b7ac3b0f05
nizam <nizamtech@aamartaka.com>
2022-06-19 19:59:44 +0600

    6/3/22 last push

--- src/components/Mobile/VisitedCompany/CompanyDetails/CompanyDetails.js
+++ src/components/Mobile/VisitedCompany/CompanyDetails/CompanyDetails.js
@@ -6,8 +6,12 @@ const CompanyDetails = ({ id }) => {
     <div>
       <BackMenu title={id} />
 
-      <div className=" bg-gray-100">
-        <h1> {id} </h1>
+      <div className="  absolute bg-gray-200 w-full h-screen">
+        <div className="myShadow rounded-lg mx-2 my-2 bg-white">
+          <h1 className="bg-[#4DCBBF] py-2 text-lg text-center ">{id}</h1>
+          <h1>{id}</h1>
+          <h1>{id}</h1>
+        </div>
       </div>
     </div>
   );
