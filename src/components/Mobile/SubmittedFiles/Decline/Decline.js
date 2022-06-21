8852533712df483a32c33246399d05c1d1855228
nizam <nizamtech@aamartaka.com>
2022-06-21 15:57:02 +0600

    decline is done

--- src/components/Mobile/SubmittedFiles/Decline/Decline.js
+++ src/components/Mobile/SubmittedFiles/Decline/Decline.js
@@ -1,8 +1,9 @@
 import React from "react";
 import BackMenu from "../../../Shared/BackMenu";
+import LeadListCard from "../../../Shared/LeadListCard/LeadListCard";
 import ApproedCard from "../Approved/ApproedCard";
 import DeclineCard from "./DeclineCard";
-
+import data from "../../../../../public/assets/data/data.json";
 const Decline = () => {
   const approvedData = [
     {
@@ -58,20 +59,24 @@ const Decline = () => {
     <div>
       <BackMenu title="Decline" />
       <div className="  absolute bg-gray-200 w-full h-screen">
-        <div className="myShadow bg-white h-screen my-2 rounded-lg p-2">
-          <h1 className=" font-monster text-ms mb-2 font-bold">Decline List</h1>
-
+        <div className="myShadow bg-white my-2 rounded-lg p-2">
           <div>
-            {approvedData &&
-              approvedData.map((data) => {
+            {data &&
+              data.map((item) => {
                 return (
                   <>
-                    <details>
-                      <summary className=" shadow py-2 px-3 bg-gray-50 my-1 font-monster rounded-sm">
-                        {data.productType}
-                      </summary>
-                      <DeclineCard {...data} />
-                    </details>
+                    <div className="rounded-md m-2 bg-white myShadow py-2">
+                      <div className="mx-4 flex justify-between items-center">
+                        <h1 className="  text-sm font-monster bg-sky-500 px-2 text-white rounded-md py-[2px]">
+                          {item?.productType}
+                        </h1>
+
+                        {/* <h1 className="  text-sm font-monster ">2,000,000</h1> */}
+                      </div>
+                      <hr className=" border-dotted  my-1 border-gray-400" />
+                      <LeadListCard item={item} />
+                      <DeclineCard {...item} />
+                    </div>
                   </>
                 );
               })}
--- src/components/Mobile/SubmittedFiles/Decline/DeclineCard.js
+++ src/components/Mobile/SubmittedFiles/Decline/DeclineCard.js
@@ -1,32 +1,28 @@
 import React from "react";
 
-const DeclineCard = (data) => {
-  const { companyName, name, trackID, date, limit, productType, reason } = data;
+const DeclineCard = (item) => {
+  const { reason, sendBack } = item;
   return (
-    <div className="   my-2 ">
-      {/* <h1 className=" font-monster font-bold bg-sky-500 text-white p-2 text-center rounded-t-lg">
-        {companyName}
-      </h1> */}
-      <div className=" grid grid-cols-12 f gap-2 p-2 font-roboto">
-        <div className=" col-span-5 text-slate-500 text-sm">
-          <h1>Customer Name</h1>
-          <h1>Track ID</h1>
-          <h1>Date</h1>
-
-          {/* <h1>Product Type</h1> */}
-          {/* <h1>Customer Address</h1> */}
-        </div>
-        <div className=" col-span-7 text-slate-700 font-bold text-sm">
-          <h1>{name}</h1>
-          <h1>{trackID}</h1>
-          <h1>{date}</h1>
-        </div>
+    <details className=" mx-3 my-2 font-monster ">
+      <summary className=" bg-slate-100 w-fu p-2 ">Decline Details</summary>
+      <div className="mt-3">
+        {sendBack &&
+          sendBack?.map((item, index) => {
+            return (
+              <>
+                <div className=" font-roboto text-justify py-1 grid grid-cols-12 ">
+                  <h1 className=" col-span-1">
+                    <span className=" text-slate-400">{index + 1}.</span>{" "}
+                  </h1>
+                  <h1 className=" col-span-11">
+                    <span className="">{item}</span>
+                  </h1>
+                </div>
+              </>
+            );
+          })}
       </div>
-      <div className="mx-2">
-        <h1>Reason</h1>
-        <h1 className=" font-roboto text-red-600 text-justify">{reason}</h1>
-      </div>
-    </div>
+    </details>
   );
 };
 
