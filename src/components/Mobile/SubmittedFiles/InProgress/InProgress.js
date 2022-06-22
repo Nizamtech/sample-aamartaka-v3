7bf3f63dcf1af0fb9a575601632a8fcf2db213b0
nizam <nizamtech@aamartaka.com>
2022-06-22 12:55:23 +0600

    global bg change

--- src/components/Mobile/SubmittedFiles/InProgress/InProgress.js
+++ src/components/Mobile/SubmittedFiles/InProgress/InProgress.js
@@ -8,7 +8,7 @@ const InProgress = () => {
     <div>
       <BackMenu title="In Progress" />
       <div className="  absolute bg-gray-200 w-full h-screen">
-        <div className="myShadow bg-white h-screen my-2 rounded-lg p-2">
+        <div className="myShadow bg-white my-2 rounded-lg p-2">
           {data &&
             data.map((item) => {
               return (
