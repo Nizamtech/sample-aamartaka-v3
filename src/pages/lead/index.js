2b461cbbb6bcf80e550b1a87c5d4d2367f8c2d02
nizam <nizamtech@aamartaka.com>
2022-06-22 12:58:47 +0600

    bug fixed

--- src/pages/lead/index.js
+++ src/pages/lead/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Mobile_Dashboard from "../../components/Mobile/Home/Mobile_Dashboard";
 import MobileLead from "../../components/Mobile/Lead/MobileLead";
 
 const index = ({ isMobile }) => {
