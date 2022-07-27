d153f50bec6ad0b4afdcc66195b2749581157307
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-07-27 19:11:22 +0600

    Home Bannar Design added

--- src/components/Desktop/Home/Desktop_Home.js
+++ src/components/Desktop/Home/Desktop_Home.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import About from '../About/About';
 import Company_Status from '../CompanyStatus/Company_Status';
-import Dashboard from '../Dashboard/Dashboard';
+import Desktop_Bannar from '../Desktop_Bannar/Desktop_Bannar';
 import Others_Bank from '../Others_Bank/Others_Bank';
-import Pictiure from '../Picture/Pictiure';
-import States from '../States/States';
 import Submitted_Files from '../Submitted_Files/Submitted_Files';
-import User from '../User/User';
+
 
 const Desktop_Home = () => {
     return (
         <>
             {/* <Dashboard /> */}
+            <Desktop_Bannar />
             <Company_Status />
             <Submitted_Files />
             <Others_Bank />
