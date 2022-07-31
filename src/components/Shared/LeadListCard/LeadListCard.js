b7b9fde48db74d19ed32a56b2ea5e8d081174a9d
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-07-31 16:39:41 +0600

    In Progress Page Design Complete

--- src/components/Shared/LeadListCard/LeadListCard.js
+++ src/components/Shared/LeadListCard/LeadListCard.js
@@ -1,18 +1,25 @@
+import { faBan, faCheck, faSpinner, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 const LeadListCard = ({ item, isStatus }) => {
   return (
     <div className="px-3">
       <div
         className={`flex ${isStatus ? "justify-between" : "justify-end"
-          }  items-center mx-4`}
-      >
+          }  items-center`}>
         {isStatus && (
           <h1
-            className={`${item?.status === "Rejected" && "text-red-500"} ${item?.status === "Aproved" && "text-green-500"
-              } ${item?.status === "Declined" && " text-orange-600"}  ${item?.status === "In Process" && " text-yellow-500"
-              }   font-monster font-bold text-sm`}
+            className={`${item?.status === "Rejected" && "text-red-500"} ${item?.status === "Aproved" && "text-green-600"
+              } ${item?.status === "Declined" && " text-yellow-600"}  ${item?.status === "In Process" && " text-cyan-500"
+              } font-monster font-bold text-sm`}
           >
-            {item?.status}
+            <div className="flex items-center">
+              {item?.status === "Rejected" && <FontAwesomeIcon icon={faBan} className="text-red-500 h-5 w-5 mr-1 font-bold" />}
+              {item?.status === "Aproved" && <FontAwesomeIcon icon={faCheck} className="text-green-700 h-5 w-5 mr-1 font-bold" />}
+              {item?.status === "Declined" && <FontAwesomeIcon icon={faXmark} className="text-yellow-600 h-5 w-5 mr-1 font-bold" />}
+              {item?.status === "In Process" && <FontAwesomeIcon icon={faSpinner} className="text-cyan-500 h-5 w-5 mr-1 font-bold" />}
+              {item?.status}
+            </div>
           </h1>
         )}
       </div>
@@ -24,7 +31,7 @@ const LeadListCard = ({ item, isStatus }) => {
         <div className="text-md text-black font-semibold">
           <h5>
             Bank:
-            <span className="font-bold ml-1">07,854</span>
+            <span className="font-bold ml-1">7,854</span>
           </h5>
           <h5>
             Cash:
