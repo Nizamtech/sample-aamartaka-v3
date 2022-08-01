16943f920cf8c82abc468c51246a6b906505ff31
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-08-01 15:26:16 +0600

    Home Page Design Complete

--- src/components/Mobile/Home/Home_Header.js
+++ src/components/Mobile/Home/Home_Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home_Header = () => {
   return (
-    <div className="relative">
+    <div className="relative pb-2">
       <div className="gradientBG px-4 pb-12 pt-7 font-monster rounded-br-[40%]">
         <div className="flex justify-between items-center py-5 -mt-6 px-2">
           <div>
@@ -30,18 +30,18 @@ const Home_Header = () => {
           </button>
         </div>
       </div>
-      <div className="absolute top-40 pt-3 pb-2 grid rounded-md grid-cols-2 place-content-between place-items-center shadow-md bg-gray-50 w-2/3 left-[20%] border border-sky-400">
+      <div className="absolute top-40 pt-3 pb-2 grid rounded-md grid-cols-2 place-content-between place-items-center shadow-md bg-gray-50 w-2/3 left-[20%] border border-sky-200">
         <div className="flex flex-col justify-between items-center w-full border-r border-r-sky-800">
           <h1 className=" font-bold text-black font-monster">
             Target Points
           </h1>
-          <h1 className="text-md font-exo text-black font-bold tracking-[2px] border-4 border-sky-400 rounded-full p-1 h-9 w-9">15</h1>
+          <h1 className="text-md font-exo text-black font-bold tracking-[2px] border-4 border-sky-500 rounded-full p-1 h-9 w-9">15</h1>
         </div>
         <div className="flex flex-col justify-between items-center ">
           <h1 className="font-bold text-black font-monster">
             Achive Points
           </h1>
-          <h1 className="text-md font-exo text-black font-bold tracking-[2px] border-4 border-sky-400 rounded-full p-1 h-9 w-9 ">
+          <h1 className="text-md font-exo text-black font-bold tracking-[2px] border-4 border-sky-500 rounded-full p-1 h-9 w-9 ">
             10
           </h1>
         </div>
