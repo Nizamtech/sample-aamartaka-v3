ee7fe5b7a8e7910b1515ba06969a0c60a3aebb83
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-08-02 17:09:28 +0600

    Home Page Design Issue Fixed

--- src/components/Mobile/Home/Mobile_Bannar.js
+++ src/components/Mobile/Home/Mobile_Bannar.js
@@ -3,11 +3,11 @@ const Mobile_Bannar = () => {
     return (
         <div className="mx-2">
             <div className="bg-[#226CD3] rounded-t-md mb-4 bannar">
-                <div className="flex flex-col items-center justify-center pt-7 pb-10">
-                    <p className="text-gray-300 text-center font-exo tracking-wider text-lg">Total Earning</p>
-                    <h2 className="text-white text-center text-3xl font-[700] tracking-wider mt-1 mb-4">BDT 10,000.00</h2>
+                <div className="py-7">
+                    <p className="text-gray-300 text-center tracking-wide text-lg">Total Earning</p>
+                    <h2 className="text-white text-center text-3xl font-[700] tracking-wide mt-2 mb-5">BDT 10,000.00</h2>
                     <div className="w-44 mx-auto">
-                        <button type='button' className=" border border-gray-100 w-44 py-2 rounded-full text-white text-xl font-semibold tracking-wider">Withdraw</button>
+                        <button type='button' className="border border-gray-100 w-44 py-2 rounded-full text-white text-xl font-semibold tracking-wide">Withdraw</button>
                     </div>
                 </div>
             </div>
